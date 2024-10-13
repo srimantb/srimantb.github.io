de62a7ec1fcf90678b96e3fce756b2b63878950c
srimant <srimantk666@gmail.com>
2024-10-13 18:38:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+    this.classList.toggle('open');
+    document.getElementById('nav-links').classList.toggle('show');
+});
+
